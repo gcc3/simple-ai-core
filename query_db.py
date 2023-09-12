ae3ff6f64529870ce3cf4c6e1ea64c20683d233f
Heyang Liu <lhypds@gmail.com>
2023-09-12 08:00:50 +0000

    Fix query initialize

--- query_db.py
+++ query_db.py
@@ -1,37 +1,36 @@
 import os
 import sys
 
+from dotenv import load_dotenv, find_dotenv
 from langchain import OpenAI, SQLDatabase, SQLDatabaseChain
 from langchain.chat_models import ChatOpenAI
 
-from dotenv import load_dotenv, find_dotenv
-load_dotenv(find_dotenv())
-
-openai_api_key = os.environ["OPENAI_API_KEY"]
-model_name = os.environ["MODEL_NAME"]
-temperature = os.environ["TEMPERATURE"]
-db_username = os.environ["DB_USERNAME"]
-db_password = os.environ["DB_PASSWORD"]
-db_host = os.environ["DB_HOST"]
-db_port = os.environ["DB_PORT"]
-db_database = os.environ["DB_DATABASE"]
-
-db_uri = f"mysql+pymysql://{db_username}:{db_password}@{db_host}:{db_port}/{db_database}"
-db = SQLDatabase.from_uri(db_uri)
-
-llm = ChatOpenAI(openai_api_key=openai_api_key, model_name=model_name, temperature=temperature)
-
-# verbose=True for debugging
-use_verbose = os.environ["USE_VERBOSE"] == "true"
-db_chain = SQLDatabaseChain(llm=llm, database=db, verbose=use_verbose)
-
-PROMPT = """ 
-Given an input question, first create a syntactically correct MySQL query to run,  
-then look at the results of the query and return the answer.  
-The question: {question}
-"""
-
 def process_query(query):
+    load_dotenv(find_dotenv())
+    openai_api_key = os.environ["OPENAI_API_KEY"]
+    model_name = os.environ["MODEL_NAME"]
+    temperature = os.environ["TEMPERATURE"]
+    db_username = os.environ["DB_USERNAME"]
+    db_password = os.environ["DB_PASSWORD"]
+    db_host = os.environ["DB_HOST"]
+    db_port = os.environ["DB_PORT"]
+    db_database = os.environ["DB_DATABASE"]
+
+    db_uri = f"mysql+pymysql://{db_username}:{db_password}@{db_host}:{db_port}/{db_database}"
+    db = SQLDatabase.from_uri(db_uri)
+
+    llm = ChatOpenAI(openai_api_key=openai_api_key, model_name=model_name, temperature=temperature)
+
+    # verbose=True for debugging
+    use_verbose = os.environ["USE_VERBOSE"] == "true"
+    db_chain = SQLDatabaseChain(llm=llm, database=db, verbose=use_verbose)
+
+    PROMPT = """ 
+    Given an input question, first create a syntactically correct MySQL query to run,  
+    then look at the results of the query and return the answer.  
+    The question: {question}
+    """
+
     result = db_chain.run(PROMPT.format(question=query))
     return result
 
--- query_text.py
+++ query_text.py
@@ -2,31 +2,29 @@ import os
 import sys
 
 from dotenv import load_dotenv, find_dotenv
-load_dotenv(find_dotenv())
-
 from langchain.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
-
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
-
-model_name = os.environ["MODEL_NAME"]
-temperature = os.environ["TEMPERATURE"]
-model = ChatOpenAI(model_name=model_name, temperature=temperature, streaming=False)
-
-# load documents
-textLoader = TextLoader('./data.txt', autodetect_encoding=True)
-
-# create index from loader
-index = VectorstoreIndexCreator().from_loaders([textLoader])
-
-PROMPT = """ 
-{question}
-"""
 
 # with LLM, with outside data
 # Note, if without LLM, the output is from custom data only
 def process_query(query):
+    load_dotenv(find_dotenv())
+    model_name = os.environ["MODEL_NAME"]
+    temperature = os.environ["TEMPERATURE"]
+    model = ChatOpenAI(model_name=model_name, temperature=temperature, streaming=False)
+
+    # load documents
+    textLoader = TextLoader('./data.txt', autodetect_encoding=True)
+
+    # create index from loader
+    index = VectorstoreIndexCreator().from_loaders([textLoader])
+
+    PROMPT = """ 
+    {question}
+    """
+
     result = index.query(PROMPT.format(question=query), llm=model)
     return result
 
